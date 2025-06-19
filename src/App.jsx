66e7ceca6cca0382e231f616d435dcd21e820730
Arsen.Danilov <arsen.danilov@ite.group>
2025-06-19 16:38:36 +0300

    Fix: Correct display of infographic component

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,17 @@ function App() {
   // ];
 
   const data = [
-    { id: 1, icon: "user", number: 210, about: "компаний из 8 стран" },
-    { id: 2, icon: "users", number: 8190, about: "посетителей" },
-    { id: 4, icon: "chair", number: `35 000+`, about: "кв. м"},
+    { id: 1, icon: "user", number: 210, about: "компаний из 8 стран221213" },
+    { id: 2, icon: "users", number: 81902221212123, about: "" },
+    { id: 4, icon: "chair", number: `35 000+`, about: "региона России"},
     { id: 5, icon: "chair", number: `Тетрапентанпергидрофенантрен`, about: "Отдельный сектор – Робототехника"},
+    { id: 5, icon: "chair", number: `я люблю футбол`, about: ""},
     { id: 5, icon: "chair", number: ``, about: "Новая выставка ExpoCifra"},
+    { id: 5, icon: "chair", number: ``, about: "Новая выставка ExpoCifra"},
+    { id: 5, icon: "chair", number: ``, about: "Новая выставка ExpoCifra"},
+    { id: 5, icon: "chair", number: ``, about: "Новая выставка ExpoCifra"},
+    { id: 5, icon: "chair", number: `NEW: Оборудование для производства`, about: "металлической мебели, стеллажей и дверей"},
+    { id: 4, icon: "chair", number: `Примите участие в секторе`, about: "«Мебельные комплектующие и фурнитура»"},
   ];
 
   return (
--- src/Infographic.jsx
+++ src/Infographic.jsx
@@ -1,10 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as styles from "./infographic.module.scss";
-
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
@@ -12,26 +10,42 @@ library.add(fas);
 
 const Infographic = ({ data }) => {
   config.autoAddCss = false;
+
+  const [isXxxl, setIsXxxl] = useState(window.matchMedia('(min-width: 1400px)').matches);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(min-width: 1400px)');
+
+    const handleChange = (e) => {
+      setIsXxxl(e.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleChange);
+    };
+  }, []);
+
   return (
     <div className={styles.infographic}>
       {data
         .sort((first, last) => first.sort - last.sort)
         .map((item) => {
 
+          const shouldAlignLeft = isXxxl && data.length > 6;
+          
           return (
-            <div
-              className={styles.item}
-              key={item.id}
-            >
+            <div className={`${styles.item} ${shouldAlignLeft ? styles['item__alignLeft'] : ''}`} key={item.id}>
               <div className={styles.icon}>
-                <FontAwesomeIcon icon={item.icon} size="2em" />
-              </div>
-              <div className={styles.text__container}>
-                <div className={styles.text}>
-                  <span className={styles.number}>{item.number}</span>
-                  <span className={styles.title}>{item.about}</span>
+                  <FontAwesomeIcon icon={item.icon} size="2em" />
                 </div>
-              </div>
+                <div className={styles.text__container}>
+                  <div className={styles.text}>
+                    <span className={styles.number}>{item.number}</span>
+                    <span className={styles.title}>{item.about}</span>
+                  </div>
+                </div>
             </div>
           );
         })}
